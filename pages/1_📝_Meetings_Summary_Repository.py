4038dd0e626ab70277047efeaf57f157b0e5f3ed
delphineloke-ecopetrol <delphine.loke@ecopetrol.asia>
2025-01-31 16:28:44 +0800

    Add pages and requirements.txt

---
+++ pages/1_📝_Meetings_Summary_Repository.py
@@ -0,0 +1,113 @@
+import pathlib
+import streamlit as st
+import pandas as pd
+import os
+
+root_start_path = pathlib.Path.home()
+file_path = r"\data\summaries.csv"
+csv_path = os.path.join(root_start_path, file_path)
+
+df = pd.read_csv(csv_path)
+df['Date'] = pd.to_datetime(df['Date'], format='mixed').dt.date
+counterparties = df['Counterparty'].unique()
+ 
+# Streamlit App Setup
+st.set_page_config(page_title="Meetings Summary Repository", page_icon="📝", layout="wide")
+st.title("Meetings Summary Repository 📝")
+st.write(" ### Create, update and delete meeting summary entries here!")
+# Display DataFrame
+st.dataframe(df)
+
+# Main Options
+option = st.selectbox(
+    "Do you want to create a new entry, update an existing entry, or delete an existing entry?",
+    ("Create New", "Update Existing", "Delete Existing")
+)
+
+if "attendees" not in st.session_state:
+    st.session_state.attendees = []
+
+def add_attendee():
+    st.session_state.attendees.append([name, designation, contact, email])
+    st.session_state.namewidget = ""
+    st.session_state.designationwidget = ""
+    st.session_state.contactwidget = ""
+    st.session_state.emailwidget = ""
+    
+if option == "Delete Existing":
+    summ_id = st.number_input("Enter Summary ID to delete", step=1, value=None)
+    if summ_id in df['Summary ID'].values:
+        row_index = df[df['Summary ID'] == summ_id].index[0]
+        filtered_row = df.iloc[[row_index]]
+        st.write("Entry to delete:")
+        st.dataframe(filtered_row)
+ 
+        if st.button("Delete Entry"):
+            df.drop(index=row_index, inplace=True)
+            df.to_csv(csv_path, index=False)
+            st.success(f"Successfully deleted entry with Summary ID {summ_id}!")
+    else:
+        if summ_id != None:
+            st.warning("Summary ID not found in the dataset!")
+
+elif option == "Create New":
+    
+    new_date = st.date_input("Date:")
+    new_counterparty = st.text_input("Enter Counterparty Name:")
+    summary = st.text_area("Meeting Summary:")
+    st.write("Add new Attendee")
+
+    name = st.text_input("Enter Counterparty Attendees:", key="namewidget")
+    designation = st.text_input("Enter Counterparty Designations:", key="designationwidget")
+    contact = st.text_input("Enter Counterparty Contact Numbers:", key="contactwidget")
+    email = st.text_input("Enter Counterparty Emails:", key="emailwidget")
+    
+    if st.button("Add Attendee", on_click=add_attendee):
+        st.dataframe(pd.DataFrame(st.session_state.attendees, columns=["Name", "Designation", "Contact", "Email"]))
+        st.success("Attendee added.")
+
+    if st.button("Add Entry"):
+        new_attendees = []
+        for particulars in st.session_state.attendees:
+            new_attendees.append(", ".join(particulars))
+        new_attendees = " | ".join(new_attendees)
+        new_entry = {
+            "Summary ID": df["Summary ID"].max() + 1 if not df.empty else 1,
+            "Date": new_date,
+            "Counterparty": new_counterparty,
+            "Summary": summary,
+            "Attendee(s) Details": new_attendees
+        }
+
+        df = pd.concat([df, pd.DataFrame([new_entry])], ignore_index=True)
+        df.to_csv(csv_path, index=False)
+        st.success("Successfully added new entry!")
+        st.dataframe(df)
+ 
+elif option == "Update Existing":
+    summ_id = st.number_input("Enter Summary ID to edit", step=1, value=None)
+    if summ_id in df['Summary ID'].values:
+        row_index = df[df['Summary ID'] == summ_id].index[0]
+        selected_row = df.loc[row_index]
+ 
+        st.write("Current Entry:")
+        st.dataframe(selected_row.to_frame().T)
+ 
+        # Allow Updates
+        new_date = st.date_input("New Date:", value=selected_row['Date'])
+        new_counterparty = st.text_input("New Counterparty Name:", value=selected_row['Counterparty'])
+        summary = st.text_area("New Meeting Summary:", value=selected_row['Summary'])
+        attendees = st.text_input("New Attendee(s) Details :", value=selected_row['Attendee(s) Details'])
+
+        if st.button("Update Entry"):
+            df.loc[row_index, "Date"] = new_date
+            df.loc[row_index, "Counterparty"] = new_counterparty
+            df.loc[row_index, "Summary"] = summary
+            df.loc[row_index, "Attendee(s) Details"] = attendees
+
+            df.to_csv(csv_path, index=False)
+            st.success(f"Successfully updated entry with Summary ID {summ_id}!")
+            st.dataframe(df)
+    else:
+        if summ_id != None:
+            st.warning("Summary ID not found in the dataset!")
---
+++ pages/2_🏭_Turnarounds_Database.py
@@ -0,0 +1,139 @@
+import streamlit as st
+import pandas as pd
+import datetime as dt
+
+ta_database_path = r"\data\ta_database.csv"
+daily_ta_path = r"\data\daily_ta.csv"
+
+ta_database = pd.read_csv(ta_database_path).sort_values(by=["start_date", "end_date"], ascending=[False, False])
+ta_database['start_date'] = pd.to_datetime(ta_database['start_date'], format='mixed').dt.date
+ta_database['end_date'] = pd.to_datetime(ta_database['end_date'], format='mixed').dt.date
+daily = pd.read_csv(daily_ta_path, low_memory=False)
+ref_lst = ta_database["Name"].sort_values(ascending=True).unique()
+
+def get_ref_id(refinery, col):
+    row_index = ta_database[ta_database['Name'] == refinery].index[0]
+    id = ta_database.at[row_index, col]
+    return id
+
+def make_daily_ta(ta_database_path, daily_ta_path):
+    expanded_rows = []
+    outages_grouped_df = pd.read_csv(ta_database_path)
+    for _, row in outages_grouped_df.iterrows():
+        date_range = pd.date_range(start=row["start_date"], end=row["end_date"])
+        for date in date_range:
+            expanded_rows.append({
+                "Outage ID": row["Outage ID"],
+                "Platts Outage ID": row["Platts Outage ID"],
+                "ECPTA ID": row["ECPTA ID"],
+                "Name": row["Name"],
+                "Country": row["Country"],
+                "Planning Status": row["Planning Status"],
+                "Unit": row["Unit"],
+                "Date": date,
+                "Volume": row["Volume"]
+            })
+    
+    expanded_df = pd.DataFrame(expanded_rows)
+    expanded_df.to_csv(daily_ta_path, index=False)
+
+# Streamlit App
+st.set_page_config(page_title="Turnarounds Database 🏭", page_icon="🏭", layout="wide")
+st.title("Turnarounds Database 🏭")
+st.write(" ### Create, update and delete refinery turnaround entries here!")
+
+# Display DataFrame (optional)
+st.write("Initial Database")
+st.dataframe(ta_database)
+
+st.subheader("Search by Refinery")
+refinery_name = st.selectbox("Select Refinery: ", ref_lst)
+
+if refinery_name in ta_database['Name'].values:
+        filtered_grouped = ta_database[(ta_database['Name'] == refinery_name)]
+        st.dataframe(filtered_grouped)
+else:
+     st.error("Refinery not found.")
+
+st.subheader("Update Refinery Turnaround Entries")
+option = st.selectbox("Do you want to create a new entry, update an existing entry, or delete an existing entry for this refinery?", 
+                      ("Create New", "Update Existing", "Delete Existing"))
+entries = []
+# Create New Entry
+if option == "Create New":
+    new_start_date = st.date_input("Start Date: ", "today", format="YYYY-MM-DD")
+    new_end_date = st.date_input("End Date: ", "today", format="YYYY-MM-DD")
+    new_status = st.selectbox("Planning Status: ", ("Planned", "Unplanned"))
+    new_unit = st.selectbox("Unit: ", ("CDU", "HCU", "FCU", "Coker", "Reformer"))
+    new_volume = st.number_input("Outage Volume (KBD): ", value=None)
+    new_outage_id = max(ta_database['Outage ID']) + 1
+    new_ref_id = get_ref_id(refinery_name, 'ECPTA ID')
+    new_country = get_ref_id(refinery_name, 'Country')
+
+    row ={'Outage ID': new_outage_id, 'Platts Outage ID': '', 'ECPTA ID': new_ref_id, 'Name': refinery_name, 'Country': new_country, 'Planning Status': new_status, 'Unit': new_unit, 
+           'start_date': new_start_date, 'end_date':new_end_date, 'Volume': new_volume, 'Edited': 'Yes'}
+    entries.append(row)
+
+    if st.button("Add Turnaround to Database"):
+        entries_df = pd.DataFrame(entries, columns=['Outage ID', 'Platts Outage ID', 'ECPTA ID', 'Name', 'Country', 'Planning Status', 'start_date', 'end_date',
+                                                                          'Volume', 'Edited'])
+        if new_end_date >= new_start_date:
+            st.dataframe(entries_df)   
+            ta_database = pd.concat([ta_database, entries_df])  
+            st.success("Entry successfully inserted into Database!")
+
+            ta_database.to_csv(ta_database_path, index=False)
+            make_daily_ta(ta_database_path, daily_ta_path)
+
+# Delete Existing Entry
+elif option == "Delete Existing":
+    outage_id = st.number_input("Enter Outage ID to delete", step=1, value=None)
+    if outage_id in ta_database["Outage ID"].values:
+        row_index = ta_database[ta_database['Outage ID'] == outage_id].index[0]
+        filtered_row = ta_database.loc[row_index]
+        st.write("Entry to delete:")
+        st.dataframe(filtered_row)
+        
+        if st.button("Delete Entry"):
+            ta_database.drop(index=row_index, inplace=True)
+            ta_database.to_csv(ta_database_path, index=False)
+            st.success(f"Successfully deleted entry with Outage ID {outage_id}!")
+    else:
+        if outage_id != None:
+            st.warning("Summary ID not found in the dataset!")
+
+# Update Existing Entry
+else:
+    outage_id = st.number_input("Enter Outage ID to replace", step=1, value=None)
+    
+    if outage_id in ta_database['Outage ID'].values:
+        row_index = ta_database[ta_database['Outage ID'] == outage_id].index[0]
+        selected_row = ta_database.loc[row_index]
+
+        st.write("Current Entry:")
+        st.dataframe(selected_row.to_frame().T)
+
+        new_status = st.selectbox("Planning Status: ", ("Planned", "Unplanned"))
+        new_unit = st.selectbox("Unit: ", ("CDU", "HCU", "FCU", "Coker", "Reformer"))
+        new_start_date = st.date_input("Start Date: ", "today", format="YYYY-MM-DD")
+        new_end_date = st.date_input("End Date: ", "today", format="YYYY-MM-DD")
+        new_volume = st.number_input("Outage Volume (KBD): ", value=selected_row['Volume'])
+
+        if st.button("Update Entry"):
+        
+                ta_database.at[row_index, 'start_date'] = new_start_date
+                ta_database.at[row_index, 'end_date'] = new_end_date
+                ta_database.at[row_index, 'Volume'] = new_volume
+                ta_database.at[row_index, 'Edited'] = 'Yes'
+                filtered_grouped = ta_database[(ta_database['Name'] == refinery_name)]
+                filtered_grouped = filtered_grouped.sort_values(by=['start_date', 'end_date', 'Outage ID', 'Edited'], ascending=[ False, False, True, False])
+                st.success("Existing entry updated in database!")
+                st.dataframe(filtered_grouped)
+
+                ta_database.to_csv(ta_database_path, index=False)
+                make_daily_ta(ta_database_path, daily_ta_path)
+    else:
+        if outage_id != None:
+            st.warning("Summary ID not found in the dataset!")
+
+    
